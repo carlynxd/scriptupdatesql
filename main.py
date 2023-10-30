5140c62a0de20128c58c4807e5107916f9337720
Carlos <carlos@ddsinfo.com.br>
2023-10-30 11:58:15 -0300

    update

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import pandas as pd
 
 arquivo = 'PLANILHA MOTORISTAS.xlsx'
 
-df = pd.read_excel(arquivo)
+dfsheets = pd.read_excel(arquivo, sheet_name=None)
 
-cpf = df['CPF']
+cpf2 = []
 
-for num in range(len(cpf)):
-    control = cpf[num]
-    cpf[num] = control.replace(".", "").replace("-", "").replace("/", "")
+for sheets, df in dfsheets.items():
+    cpf = df['CPF']
+    print(sheets)
+    for num in range(len(cpf)):
+        control = str(cpf[num])
+        control = control.replace(".", "").replace("-", "").replace("/", "").replace(" ", "").replace(" ", "")
+        cpf2.append(control)
+        print(cpf2)
 
-print(cpf)
 
 i = 0
-with open("UPDATE", 'w') as arquivo:
+with open("UPDATE.TXT", 'w') as arquivo:
     arquivo.write(f"update tb_motorista set motorista_ativo = 'S' where MOTORISTA_CADASTRO IN (")
-    for i in range(len(cpf)):
-        if i == len(cpf) - 1:
-            arquivo.write(f"{cpf[i]})")
+    for i in range(len(cpf2)):
+        if i == len(cpf2) - 1:
+            arquivo.write(f"'{cpf2[i]}')")
         else:
-            arquivo.write(f"{cpf[i]},")
+            arquivo.write(f"'{cpf2[i]}',\n")
 
